bfd77dbf801dafcc79ca326e91ca9297ac78b02a
Victor Tang <victortang1992@gmail.com>
2016-01-04 01:51:44 -0800

    comment on gallery load

--- routes/views/gallery.js
+++ routes/views/gallery.js
@@ -8,7 +8,7 @@ exports = module.exports = function(req, res) {
 	// Set locals
 	locals.section = 'gallery';
 	
-	// Load the galleries by sortOrder
+	// Load all Gallery's into 'galleries', ordered by by sortOrder
 	view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
 	
 	// Render the view
