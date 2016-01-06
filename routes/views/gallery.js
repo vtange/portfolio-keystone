f99caf33f3b0885a0efe7523192b4ea8075c4dc5
Victor Tang <victortang1992@gmail.com>
2016-01-06 01:39:40 -0800

    client side galleryApp.js

--- routes/views/gallery.js
+++ routes/views/gallery.js
@@ -4,7 +4,7 @@ exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	console.log(keystone.list('Gallery').model);//keystone.list('Gallery').model is Mongoose object that can find()
+	//console.log(keystone.list('Gallery').model);//keystone.list('Gallery').model is Mongoose object that can find()
 
 	// Set 'active' class to 'works'
 	locals.section = 'works';
