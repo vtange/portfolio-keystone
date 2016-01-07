e006648e4dda8bba4187bb397cf31e0927277703
Victor Tang <victortang1992@gmail.com>
2016-01-07 01:20:20 -0800

    comments and rename Gallery.js model

--- models/Project.js
+++ models/Project.js
@@ -2,7 +2,7 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 /**
- * Gallery Model
+ * Project Model
  * =============
  */
 
--- routes/views/project.js
+++ routes/views/project.js
@@ -4,12 +4,12 @@ exports = module.exports = function(req, res) {
 	
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	//console.log(keystone.list('Gallery').model);//keystone.list('Gallery').model is Mongoose object that can find()
+	//console.log(keystone.list('Project').model);//keystone.list('Project').model is Mongoose object that can find()
 
 	// Set 'active' class to 'works'
 	locals.section = 'works';
 	
-	// Load all Gallery's into 'galleries', ordered by by sortOrder
+	// Load all Projects into 'projects' list (defined in keystone.js), ordered by by sortOrder
 	view.query('projects', keystone.list('Project').model.find().sort('sortOrder'));
 	
 	// Render views/works.hbs
